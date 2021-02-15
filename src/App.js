22950a9e804e187e345d2299314c688c808bccde
jonath92 <jonathanheard92@gmail.com>
2021-02-15 11:01:56 +0100

    finish disclaimer, reduce image size

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Disclaimer from './pages/Disclaimer'
 import './css/app.css';
+import ScrollToTop from 'react-router-scroll-top'
 
 export default function App() {
   return (
     <div>
       <Router>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/home' component={Home} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/disclaimer' component={Disclaimer} />
+        <ScrollToTop>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/home' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/disclaimer' component={Disclaimer} />
+        </ScrollToTop>
+
 
       </Router>
     </div>
